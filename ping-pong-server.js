5bfb3cf250150590acae6fa5e0024618f0a00b31
Nikhil kumar <57635124+Nikhilkumar25@users.noreply.github.com>
2024-10-07 17:15:22 +0530

    Add files via upload

---
+++ ping-pong-server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+const waitingPlayers = [];
+const activegames = new Map();
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('findgame', (playerName) => {
+        console.log(`${playerName} is looking for a game`);
+        findgame(socket, playerName);
+    });
+
+    socket.on('paddleMove', (data) => {
+        const game = activegames.get(socket.id);
+        if (game) {
+            io.to(game.opponent).emit('opponentPaddleMove', data);
+        }
+    });
+
+    socket.on('updateBall', (ballData) => {
+        const game = activegames.get(socket.id);
+        if (game) {
+            io.to(game.opponent).emit('ballUpdate', ballData);
+        }
+    });
+
+    socket.on('updateScore', (scoreData) => {
+        const game = activegames.get(socket.id);
+        if (game) {
+            io.to(game.opponent).emit('scoreUpdate', scoreData);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+        handleDisconnect(socket);
+    });
+});
+
+function findgame(socket, playerName) {
+    if (waitingPlayers.length > 0) {
+        const opponent = waitingPlayers.pop();
+        const game = {
+            players: [socket.id, opponent.id],
+            playerNames: [playerName, opponent.playerName]
+        };
+        
+        activegames.set(socket.id, { opponent: opponent.id, game });
+        activegames.set(opponent.id, { opponent: socket.id, game });
+
+        socket.emit('gameStart', { opponentName: opponent.playerName, isHost: true });
+        opponent.socket.emit('gameStart', { opponentName: playerName, isHost: false });
+    } else {
+        waitingPlayers.push({ id: socket.id, socket, playerName });
+        socket.emit('waiting');
+    }
+}
+
+function handleDisconnect(socket) {
+    const index = waitingPlayers.findIndex(player => player.id === socket.id);
+    if (index !== -1) {
+        waitingPlayers.splice(index, 1);
+    }
+
+    const game = activegames.get(socket.id);
+    if (game) {
+        io.to(game.opponent).emit('opponentLeft');
+        activegames.delete(game.opponent);
+        activegames.delete(socket.id);
+    }
+}
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
